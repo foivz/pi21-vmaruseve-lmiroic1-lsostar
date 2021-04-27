24c63750526027fc9fddf0263c29207b9c03a363
lmiroic1 <lmiroic1@foi.hr>
2021-04-27 14:49:05 +0200

    Implementiran dizajn forme azuriranja clana.

---
+++ Software/eGym/eGym/FormaAzurirajClana.Designer.cs
@@ -0,0 +1,301 @@
+﻿
+namespace eGym
+{
+    partial class FormaAzurirajClana
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNatragAzuriranjeClana = new System.Windows.Forms.Button();
+            this.txtIznosNaRacunuA = new System.Windows.Forms.TextBox();
+            this.dtpVrijediDoA = new System.Windows.Forms.DateTimePicker();
+            this.dtpVrijediOdA = new System.Windows.Forms.DateTimePicker();
+            this.txtBrojTelefonaClanaA = new System.Windows.Forms.TextBox();
+            this.txtEmailClanaA = new System.Windows.Forms.TextBox();
+            this.txtPrezimeClanaA = new System.Windows.Forms.TextBox();
+            this.txtImeClanaA = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblVrijediDo = new System.Windows.Forms.Label();
+            this.lblVrijediOd = new System.Windows.Forms.Label();
+            this.lblClanarinaPlacena = new System.Windows.Forms.Label();
+            this.lblBrojTelefona = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblPrezime = new System.Windows.Forms.Label();
+            this.lblIme = new System.Windows.Forms.Label();
+            this.lblImeIPrezimeClana = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAzurirajClanaA = new System.Windows.Forms.Button();
+            this.cbClanarinaPlacenaAzurirajClana = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnNatragAzuriranjeClana
+            // 
+            this.btnNatragAzuriranjeClana.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnNatragAzuriranjeClana.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNatragAzuriranjeClana.ForeColor = System.Drawing.Color.White;
+            this.btnNatragAzuriranjeClana.Location = new System.Drawing.Point(81, 461);
+            this.btnNatragAzuriranjeClana.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnNatragAzuriranjeClana.Name = "btnNatragAzuriranjeClana";
+            this.btnNatragAzuriranjeClana.Size = new System.Drawing.Size(110, 47);
+            this.btnNatragAzuriranjeClana.TabIndex = 43;
+            this.btnNatragAzuriranjeClana.Text = "Natrag";
+            this.btnNatragAzuriranjeClana.UseVisualStyleBackColor = false;
+            this.btnNatragAzuriranjeClana.Click += new System.EventHandler(this.btnNatragAzuriranjeClana_Click);
+            // 
+            // txtIznosNaRacunuA
+            // 
+            this.txtIznosNaRacunuA.Location = new System.Drawing.Point(221, 379);
+            this.txtIznosNaRacunuA.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtIznosNaRacunuA.Name = "txtIznosNaRacunuA";
+            this.txtIznosNaRacunuA.Size = new System.Drawing.Size(183, 22);
+            this.txtIznosNaRacunuA.TabIndex = 42;
+            // 
+            // dtpVrijediDoA
+            // 
+            this.dtpVrijediDoA.Location = new System.Drawing.Point(517, 340);
+            this.dtpVrijediDoA.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dtpVrijediDoA.Name = "dtpVrijediDoA";
+            this.dtpVrijediDoA.Size = new System.Drawing.Size(183, 22);
+            this.dtpVrijediDoA.TabIndex = 41;
+            // 
+            // dtpVrijediOdA
+            // 
+            this.dtpVrijediOdA.Location = new System.Drawing.Point(221, 340);
+            this.dtpVrijediOdA.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dtpVrijediOdA.Name = "dtpVrijediOdA";
+            this.dtpVrijediOdA.Size = new System.Drawing.Size(183, 22);
+            this.dtpVrijediOdA.TabIndex = 40;
+            // 
+            // txtBrojTelefonaClanaA
+            // 
+            this.txtBrojTelefonaClanaA.Location = new System.Drawing.Point(221, 268);
+            this.txtBrojTelefonaClanaA.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtBrojTelefonaClanaA.Name = "txtBrojTelefonaClanaA";
+            this.txtBrojTelefonaClanaA.Size = new System.Drawing.Size(183, 22);
+            this.txtBrojTelefonaClanaA.TabIndex = 38;
+            // 
+            // txtEmailClanaA
+            // 
+            this.txtEmailClanaA.Location = new System.Drawing.Point(221, 232);
+            this.txtEmailClanaA.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtEmailClanaA.Name = "txtEmailClanaA";
+            this.txtEmailClanaA.Size = new System.Drawing.Size(183, 22);
+            this.txtEmailClanaA.TabIndex = 37;
+            // 
+            // txtPrezimeClanaA
+            // 
+            this.txtPrezimeClanaA.Location = new System.Drawing.Point(221, 195);
+            this.txtPrezimeClanaA.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtPrezimeClanaA.Name = "txtPrezimeClanaA";
+            this.txtPrezimeClanaA.Size = new System.Drawing.Size(183, 22);
+            this.txtPrezimeClanaA.TabIndex = 36;
+            // 
+            // txtImeClanaA
+            // 
+            this.txtImeClanaA.Location = new System.Drawing.Point(221, 158);
+            this.txtImeClanaA.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtImeClanaA.Name = "txtImeClanaA";
+            this.txtImeClanaA.Size = new System.Drawing.Size(183, 22);
+            this.txtImeClanaA.TabIndex = 35;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(78, 379);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 18);
+            this.label2.TabIndex = 34;
+            this.label2.Text = "Iznos na računu:";
+            // 
+            // lblVrijediDo
+            // 
+            this.lblVrijediDo.AutoSize = true;
+            this.lblVrijediDo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVrijediDo.Location = new System.Drawing.Point(426, 340);
+            this.lblVrijediDo.Name = "lblVrijediDo";
+            this.lblVrijediDo.Size = new System.Drawing.Size(72, 18);
+            this.lblVrijediDo.TabIndex = 33;
+            this.lblVrijediDo.Text = "Vrijedi do:";
+            // 
+            // lblVrijediOd
+            // 
+            this.lblVrijediOd.AutoSize = true;
+            this.lblVrijediOd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVrijediOd.Location = new System.Drawing.Point(119, 344);
+            this.lblVrijediOd.Name = "lblVrijediOd";
+            this.lblVrijediOd.Size = new System.Drawing.Size(72, 18);
+            this.lblVrijediOd.TabIndex = 32;
+            this.lblVrijediOd.Text = "Vrijedi od:";
+            // 
+            // lblClanarinaPlacena
+            // 
+            this.lblClanarinaPlacena.AutoSize = true;
+            this.lblClanarinaPlacena.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClanarinaPlacena.Location = new System.Drawing.Point(65, 307);
+            this.lblClanarinaPlacena.Name = "lblClanarinaPlacena";
+            this.lblClanarinaPlacena.Size = new System.Drawing.Size(129, 18);
+            this.lblClanarinaPlacena.TabIndex = 31;
+            this.lblClanarinaPlacena.Text = "Članarina plaćena:";
+            // 
+            // lblBrojTelefona
+            // 
+            this.lblBrojTelefona.AutoSize = true;
+            this.lblBrojTelefona.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBrojTelefona.Location = new System.Drawing.Point(99, 271);
+            this.lblBrojTelefona.Name = "lblBrojTelefona";
+            this.lblBrojTelefona.Size = new System.Drawing.Size(95, 18);
+            this.lblBrojTelefona.TabIndex = 30;
+            this.lblBrojTelefona.Text = "Broj telefona:";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(141, 235);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(54, 18);
+            this.lblEmail.TabIndex = 29;
+            this.lblEmail.Text = "E-mail:";
+            // 
+            // lblPrezime
+            // 
+            this.lblPrezime.AutoSize = true;
+            this.lblPrezime.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrezime.Location = new System.Drawing.Point(127, 200);
+            this.lblPrezime.Name = "lblPrezime";
+            this.lblPrezime.Size = new System.Drawing.Size(67, 18);
+            this.lblPrezime.TabIndex = 28;
+            this.lblPrezime.Text = "Prezime:";
+            // 
+            // lblIme
+            // 
+            this.lblIme.AutoSize = true;
+            this.lblIme.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIme.Location = new System.Drawing.Point(157, 163);
+            this.lblIme.Name = "lblIme";
+            this.lblIme.Size = new System.Drawing.Size(36, 18);
+            this.lblIme.TabIndex = 27;
+            this.lblIme.Text = "Ime:";
+            // 
+            // lblImeIPrezimeClana
+            // 
+            this.lblImeIPrezimeClana.AutoSize = true;
+            this.lblImeIPrezimeClana.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImeIPrezimeClana.Location = new System.Drawing.Point(331, 100);
+            this.lblImeIPrezimeClana.Name = "lblImeIPrezimeClana";
+            this.lblImeIPrezimeClana.Size = new System.Drawing.Size(167, 29);
+            this.lblImeIPrezimeClana.TabIndex = 26;
+            this.lblImeIPrezimeClana.Text = "Ažuriraj člana";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(58, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 55);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "eGym";
+            // 
+            // btnAzurirajClanaA
+            // 
+            this.btnAzurirajClanaA.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnAzurirajClanaA.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAzurirajClanaA.ForeColor = System.Drawing.Color.White;
+            this.btnAzurirajClanaA.Location = new System.Drawing.Point(588, 461);
+            this.btnAzurirajClanaA.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnAzurirajClanaA.Name = "btnAzurirajClanaA";
+            this.btnAzurirajClanaA.Size = new System.Drawing.Size(112, 47);
+            this.btnAzurirajClanaA.TabIndex = 44;
+            this.btnAzurirajClanaA.Text = "Ažuriraj člana";
+            this.btnAzurirajClanaA.UseVisualStyleBackColor = false;
+            this.btnAzurirajClanaA.Click += new System.EventHandler(this.btnAzurirajClanaA_Click);
+            // 
+            // cbClanarinaPlacenaAzurirajClana
+            // 
+            this.cbClanarinaPlacenaAzurirajClana.FormattingEnabled = true;
+            this.cbClanarinaPlacenaAzurirajClana.Location = new System.Drawing.Point(221, 306);
+            this.cbClanarinaPlacenaAzurirajClana.Name = "cbClanarinaPlacenaAzurirajClana";
+            this.cbClanarinaPlacenaAzurirajClana.Size = new System.Drawing.Size(183, 24);
+            this.cbClanarinaPlacenaAzurirajClana.TabIndex = 45;
+            // 
+            // FormaAzurirajClana
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 617);
+            this.Controls.Add(this.cbClanarinaPlacenaAzurirajClana);
+            this.Controls.Add(this.btnAzurirajClanaA);
+            this.Controls.Add(this.btnNatragAzuriranjeClana);
+            this.Controls.Add(this.txtIznosNaRacunuA);
+            this.Controls.Add(this.dtpVrijediDoA);
+            this.Controls.Add(this.dtpVrijediOdA);
+            this.Controls.Add(this.txtBrojTelefonaClanaA);
+            this.Controls.Add(this.txtEmailClanaA);
+            this.Controls.Add(this.txtPrezimeClanaA);
+            this.Controls.Add(this.txtImeClanaA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblVrijediDo);
+            this.Controls.Add(this.lblVrijediOd);
+            this.Controls.Add(this.lblClanarinaPlacena);
+            this.Controls.Add(this.lblBrojTelefona);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblPrezime);
+            this.Controls.Add(this.lblIme);
+            this.Controls.Add(this.lblImeIPrezimeClana);
+            this.Controls.Add(this.label1);
+            this.Name = "FormaAzurirajClana";
+            this.Text = "AzurirajClana";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnNatragAzuriranjeClana;
+        private System.Windows.Forms.TextBox txtIznosNaRacunuA;
+        private System.Windows.Forms.DateTimePicker dtpVrijediDoA;
+        private System.Windows.Forms.DateTimePicker dtpVrijediOdA;
+        private System.Windows.Forms.TextBox txtBrojTelefonaClanaA;
+        private System.Windows.Forms.TextBox txtEmailClanaA;
+        private System.Windows.Forms.TextBox txtPrezimeClanaA;
+        private System.Windows.Forms.TextBox txtImeClanaA;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblVrijediDo;
+        private System.Windows.Forms.Label lblVrijediOd;
+        private System.Windows.Forms.Label lblClanarinaPlacena;
+        private System.Windows.Forms.Label lblBrojTelefona;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblPrezime;
+        private System.Windows.Forms.Label lblIme;
+        private System.Windows.Forms.Label lblImeIPrezimeClana;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAzurirajClanaA;
+        private System.Windows.Forms.ComboBox cbClanarinaPlacenaAzurirajClana;
+    }
+}
---
+++ Software/eGym/eGym/FormaAzurirajClana.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace eGym
+{
+    public partial class FormaAzurirajClana : Form
+    {
+        public FormaAzurirajClana()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAzurirajClanaA_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Uspješno ste ažurirali člana!");
+            FormaEvidencijaClanova formaEvidencijaClanova = new FormaEvidencijaClanova();
+            formaEvidencijaClanova.Show();
+            this.Hide();
+        }
+
+        private void btnNatragAzuriranjeClana_Click(object sender, EventArgs e)
+        {
+            FormaEvidencijaClanova formaEvidencijaClanova = new FormaEvidencijaClanova();
+            formaEvidencijaClanova.Show();
+            this.Hide();
+        }
+    }
+}
