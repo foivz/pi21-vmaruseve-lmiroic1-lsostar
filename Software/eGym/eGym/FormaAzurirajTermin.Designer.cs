dba08b7ac96f632fe36e6274ad730b046e0de72d
ValentinaMarusevec1 <vmaruseve@foi.hr>
2021-04-27 16:11:32 +0200

    Implementiran dizajn forme AzuriranjeTermina.

---
+++ Software/eGym/eGym/FormaAzurirajTermin.Designer.cs
@@ -0,0 +1,274 @@
+﻿
+namespace eGym
+{
+    partial class FormaAzurirajTermin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBrojMjestaA = new System.Windows.Forms.TextBox();
+            this.txtNazivTerminaA = new System.Windows.Forms.TextBox();
+            this.btnAzurirajTermin = new System.Windows.Forms.Button();
+            this.btnNatrag = new System.Windows.Forms.Button();
+            this.cmbVrstaVjezbeA = new System.Windows.Forms.ComboBox();
+            this.cmbZaposlenikA = new System.Windows.Forms.ComboBox();
+            this.dtpDatumTerminaDoA = new System.Windows.Forms.DateTimePicker();
+            this.dtpDatumTerminaOdA = new System.Windows.Forms.DateTimePicker();
+            this.cmbVrstaTreningaA = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblRadnoVrijeme = new System.Windows.Forms.Label();
+            this.lblDatum = new System.Windows.Forms.Label();
+            this.lblImeiPrezime = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBrojMjestaA
+            // 
+            this.txtBrojMjestaA.Location = new System.Drawing.Point(224, 279);
+            this.txtBrojMjestaA.Name = "txtBrojMjestaA";
+            this.txtBrojMjestaA.Size = new System.Drawing.Size(218, 22);
+            this.txtBrojMjestaA.TabIndex = 78;
+            // 
+            // txtNazivTerminaA
+            // 
+            this.txtNazivTerminaA.Location = new System.Drawing.Point(224, 174);
+            this.txtNazivTerminaA.Name = "txtNazivTerminaA";
+            this.txtNazivTerminaA.Size = new System.Drawing.Size(218, 22);
+            this.txtNazivTerminaA.TabIndex = 77;
+            // 
+            // btnAzurirajTermin
+            // 
+            this.btnAzurirajTermin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnAzurirajTermin.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAzurirajTermin.ForeColor = System.Drawing.Color.White;
+            this.btnAzurirajTermin.Location = new System.Drawing.Point(320, 467);
+            this.btnAzurirajTermin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnAzurirajTermin.Name = "btnAzurirajTermin";
+            this.btnAzurirajTermin.Size = new System.Drawing.Size(122, 50);
+            this.btnAzurirajTermin.TabIndex = 76;
+            this.btnAzurirajTermin.Text = "Ažuriraj termin";
+            this.btnAzurirajTermin.UseVisualStyleBackColor = false;
+            this.btnAzurirajTermin.Click += new System.EventHandler(this.btnAzurirajTermin_Click);
+            // 
+            // btnNatrag
+            // 
+            this.btnNatrag.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnNatrag.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNatrag.ForeColor = System.Drawing.Color.White;
+            this.btnNatrag.Location = new System.Drawing.Point(104, 467);
+            this.btnNatrag.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnNatrag.Name = "btnNatrag";
+            this.btnNatrag.Size = new System.Drawing.Size(117, 50);
+            this.btnNatrag.TabIndex = 75;
+            this.btnNatrag.Text = "Natrag";
+            this.btnNatrag.UseVisualStyleBackColor = false;
+            this.btnNatrag.Click += new System.EventHandler(this.btnNatrag_Click);
+            // 
+            // cmbVrstaVjezbeA
+            // 
+            this.cmbVrstaVjezbeA.FormattingEnabled = true;
+            this.cmbVrstaVjezbeA.Location = new System.Drawing.Point(224, 314);
+            this.cmbVrstaVjezbeA.Name = "cmbVrstaVjezbeA";
+            this.cmbVrstaVjezbeA.Size = new System.Drawing.Size(218, 24);
+            this.cmbVrstaVjezbeA.TabIndex = 74;
+            // 
+            // cmbZaposlenikA
+            // 
+            this.cmbZaposlenikA.FormattingEnabled = true;
+            this.cmbZaposlenikA.Location = new System.Drawing.Point(224, 351);
+            this.cmbZaposlenikA.Name = "cmbZaposlenikA";
+            this.cmbZaposlenikA.Size = new System.Drawing.Size(218, 24);
+            this.cmbZaposlenikA.TabIndex = 73;
+            // 
+            // dtpDatumTerminaDoA
+            // 
+            this.dtpDatumTerminaDoA.Location = new System.Drawing.Point(224, 244);
+            this.dtpDatumTerminaDoA.Name = "dtpDatumTerminaDoA";
+            this.dtpDatumTerminaDoA.Size = new System.Drawing.Size(218, 22);
+            this.dtpDatumTerminaDoA.TabIndex = 71;
+            // 
+            // dtpDatumTerminaOdA
+            // 
+            this.dtpDatumTerminaOdA.Location = new System.Drawing.Point(224, 209);
+            this.dtpDatumTerminaOdA.Name = "dtpDatumTerminaOdA";
+            this.dtpDatumTerminaOdA.Size = new System.Drawing.Size(218, 22);
+            this.dtpDatumTerminaOdA.TabIndex = 72;
+            // 
+            // cmbVrstaTreningaA
+            // 
+            this.cmbVrstaTreningaA.FormattingEnabled = true;
+            this.cmbVrstaTreningaA.Location = new System.Drawing.Point(224, 388);
+            this.cmbVrstaTreningaA.Name = "cmbVrstaTreningaA";
+            this.cmbVrstaTreningaA.Size = new System.Drawing.Size(218, 24);
+            this.cmbVrstaTreningaA.TabIndex = 70;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(101, 391);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 18);
+            this.label6.TabIndex = 69;
+            this.label6.Text = "Vrsta treninga:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(119, 355);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 18);
+            this.label5.TabIndex = 68;
+            this.label5.Text = "Zaposlenik:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(111, 319);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 18);
+            this.label4.TabIndex = 67;
+            this.label4.Text = "Vrsta vježbe:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(116, 283);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 18);
+            this.label3.TabIndex = 66;
+            this.label3.Text = "Broj mjesta:";
+            // 
+            // lblRadnoVrijeme
+            // 
+            this.lblRadnoVrijeme.AutoSize = true;
+            this.lblRadnoVrijeme.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRadnoVrijeme.Location = new System.Drawing.Point(171, 247);
+            this.lblRadnoVrijeme.Name = "lblRadnoVrijeme";
+            this.lblRadnoVrijeme.Size = new System.Drawing.Size(32, 18);
+            this.lblRadnoVrijeme.TabIndex = 65;
+            this.lblRadnoVrijeme.Text = "Do:";
+            // 
+            // lblDatum
+            // 
+            this.lblDatum.AutoSize = true;
+            this.lblDatum.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDatum.Location = new System.Drawing.Point(171, 211);
+            this.lblDatum.Name = "lblDatum";
+            this.lblDatum.Size = new System.Drawing.Size(32, 18);
+            this.lblDatum.TabIndex = 64;
+            this.lblDatum.Text = "Od:";
+            // 
+            // lblImeiPrezime
+            // 
+            this.lblImeiPrezime.AutoSize = true;
+            this.lblImeiPrezime.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImeiPrezime.Location = new System.Drawing.Point(154, 175);
+            this.lblImeiPrezime.Name = "lblImeiPrezime";
+            this.lblImeiPrezime.Size = new System.Drawing.Size(49, 18);
+            this.lblImeiPrezime.TabIndex = 63;
+            this.lblImeiPrezime.Text = "Naziv:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(123, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 29);
+            this.label1.TabIndex = 62;
+            this.label1.Text = "Ažuriraj termin";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(76, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 55);
+            this.label2.TabIndex = 61;
+            this.label2.Text = "eGym";
+            // 
+            // FormaAzurirajTermin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 585);
+            this.Controls.Add(this.txtBrojMjestaA);
+            this.Controls.Add(this.txtNazivTerminaA);
+            this.Controls.Add(this.btnAzurirajTermin);
+            this.Controls.Add(this.btnNatrag);
+            this.Controls.Add(this.cmbVrstaVjezbeA);
+            this.Controls.Add(this.cmbZaposlenikA);
+            this.Controls.Add(this.dtpDatumTerminaDoA);
+            this.Controls.Add(this.dtpDatumTerminaOdA);
+            this.Controls.Add(this.cmbVrstaTreningaA);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblRadnoVrijeme);
+            this.Controls.Add(this.lblDatum);
+            this.Controls.Add(this.lblImeiPrezime);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Name = "FormaAzurirajTermin";
+            this.Text = "FormaAzurirajTermin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBrojMjestaA;
+        private System.Windows.Forms.TextBox txtNazivTerminaA;
+        private System.Windows.Forms.Button btnAzurirajTermin;
+        private System.Windows.Forms.Button btnNatrag;
+        private System.Windows.Forms.ComboBox cmbVrstaVjezbeA;
+        private System.Windows.Forms.ComboBox cmbZaposlenikA;
+        private System.Windows.Forms.DateTimePicker dtpDatumTerminaDoA;
+        private System.Windows.Forms.DateTimePicker dtpDatumTerminaOdA;
+        private System.Windows.Forms.ComboBox cmbVrstaTreningaA;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblRadnoVrijeme;
+        private System.Windows.Forms.Label lblDatum;
+        private System.Windows.Forms.Label lblImeiPrezime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Software/eGym/eGym/FormaAzurirajTermin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace eGym
+{
+    public partial class FormaAzurirajTermin : Form
+    {
+        public FormaAzurirajTermin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAzurirajTermin_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Uspješno ažuriran termin!");
+        }
+
+        private void btnNatrag_Click(object sender, EventArgs e)
+        {
+            FormaEvidencijaTermina formaEvidencijaTermina = new FormaEvidencijaTermina();
+            formaEvidencijaTermina.Show();
+            this.Hide();
+        }
+    }
+}
