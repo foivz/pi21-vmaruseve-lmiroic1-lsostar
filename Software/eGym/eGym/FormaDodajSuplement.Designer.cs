9fc6591cccfca9fb60a1c45937278bbdb3a4b93b
lmiroic1 <lmiroic1@foi.hr>
2021-04-27 14:48:00 +0200

    Implementiran dizajn forme FormaDodajSuplement.

---
+++ Software/eGym/eGym/FormaDodajSuplement.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace eGym
+{
+    partial class FormaDodajSuplement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDodajSuplementD = new System.Windows.Forms.Button();
+            this.btnNatragDodajSuplement = new System.Windows.Forms.Button();
+            this.lblRadnoVrijeme = new System.Windows.Forms.Label();
+            this.lblDatum = new System.Windows.Forms.Label();
+            this.lblImeiPrezime = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNazivDodajSuplement = new System.Windows.Forms.TextBox();
+            this.txtCijenaDodajSuplement = new System.Windows.Forms.TextBox();
+            this.txtKolicinaDodajSuplement = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnDodajSuplementD
+            // 
+            this.btnDodajSuplementD.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnDodajSuplementD.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDodajSuplementD.ForeColor = System.Drawing.Color.White;
+            this.btnDodajSuplementD.Location = new System.Drawing.Point(311, 345);
+            this.btnDodajSuplementD.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnDodajSuplementD.Name = "btnDodajSuplementD";
+            this.btnDodajSuplementD.Size = new System.Drawing.Size(122, 50);
+            this.btnDodajSuplementD.TabIndex = 59;
+            this.btnDodajSuplementD.Text = "Dodaj suplement";
+            this.btnDodajSuplementD.UseVisualStyleBackColor = false;
+            this.btnDodajSuplementD.Click += new System.EventHandler(this.btnDodajSuplementD_Click);
+            // 
+            // btnNatragDodajSuplement
+            // 
+            this.btnNatragDodajSuplement.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnNatragDodajSuplement.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNatragDodajSuplement.ForeColor = System.Drawing.Color.White;
+            this.btnNatragDodajSuplement.Location = new System.Drawing.Point(92, 345);
+            this.btnNatragDodajSuplement.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnNatragDodajSuplement.Name = "btnNatragDodajSuplement";
+            this.btnNatragDodajSuplement.Size = new System.Drawing.Size(117, 50);
+            this.btnNatragDodajSuplement.TabIndex = 58;
+            this.btnNatragDodajSuplement.Text = "Natrag";
+            this.btnNatragDodajSuplement.UseVisualStyleBackColor = false;
+            this.btnNatragDodajSuplement.Click += new System.EventHandler(this.btnNatragDodajSuplement_Click);
+            // 
+            // lblRadnoVrijeme
+            // 
+            this.lblRadnoVrijeme.AutoSize = true;
+            this.lblRadnoVrijeme.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRadnoVrijeme.Location = new System.Drawing.Point(98, 266);
+            this.lblRadnoVrijeme.Name = "lblRadnoVrijeme";
+            this.lblRadnoVrijeme.Size = new System.Drawing.Size(64, 18);
+            this.lblRadnoVrijeme.TabIndex = 54;
+            this.lblRadnoVrijeme.Text = "Količina:";
+            // 
+            // lblDatum
+            // 
+            this.lblDatum.AutoSize = true;
+            this.lblDatum.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDatum.Location = new System.Drawing.Point(109, 227);
+            this.lblDatum.Name = "lblDatum";
+            this.lblDatum.Size = new System.Drawing.Size(53, 18);
+            this.lblDatum.TabIndex = 53;
+            this.lblDatum.Text = "Cijena:";
+            // 
+            // lblImeiPrezime
+            // 
+            this.lblImeiPrezime.AutoSize = true;
+            this.lblImeiPrezime.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImeiPrezime.Location = new System.Drawing.Point(113, 188);
+            this.lblImeiPrezime.Name = "lblImeiPrezime";
+            this.lblImeiPrezime.Size = new System.Drawing.Size(49, 18);
+            this.lblImeiPrezime.TabIndex = 52;
+            this.lblImeiPrezime.Text = "Naziv:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(111, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 29);
+            this.label1.TabIndex = 51;
+            this.label1.Text = "Dodaj suplement";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(64, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 55);
+            this.label2.TabIndex = 50;
+            this.label2.Text = "eGym";
+            // 
+            // txtNazivDodajSuplement
+            // 
+            this.txtNazivDodajSuplement.Location = new System.Drawing.Point(215, 188);
+            this.txtNazivDodajSuplement.Name = "txtNazivDodajSuplement";
+            this.txtNazivDodajSuplement.Size = new System.Drawing.Size(218, 22);
+            this.txtNazivDodajSuplement.TabIndex = 60;
+            // 
+            // txtCijenaDodajSuplement
+            // 
+            this.txtCijenaDodajSuplement.Location = new System.Drawing.Point(215, 227);
+            this.txtCijenaDodajSuplement.Name = "txtCijenaDodajSuplement";
+            this.txtCijenaDodajSuplement.Size = new System.Drawing.Size(218, 22);
+            this.txtCijenaDodajSuplement.TabIndex = 61;
+            // 
+            // txtKolicinaDodajSuplement
+            // 
+            this.txtKolicinaDodajSuplement.Location = new System.Drawing.Point(215, 266);
+            this.txtKolicinaDodajSuplement.Name = "txtKolicinaDodajSuplement";
+            this.txtKolicinaDodajSuplement.Size = new System.Drawing.Size(218, 22);
+            this.txtKolicinaDodajSuplement.TabIndex = 62;
+            // 
+            // FormaDodajSuplement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 450);
+            this.Controls.Add(this.txtKolicinaDodajSuplement);
+            this.Controls.Add(this.txtCijenaDodajSuplement);
+            this.Controls.Add(this.txtNazivDodajSuplement);
+            this.Controls.Add(this.btnDodajSuplementD);
+            this.Controls.Add(this.btnNatragDodajSuplement);
+            this.Controls.Add(this.lblRadnoVrijeme);
+            this.Controls.Add(this.lblDatum);
+            this.Controls.Add(this.lblImeiPrezime);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Name = "FormaDodajSuplement";
+            this.Text = "DodajSuplement";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDodajSuplementD;
+        private System.Windows.Forms.Button btnNatragDodajSuplement;
+        private System.Windows.Forms.Label lblRadnoVrijeme;
+        private System.Windows.Forms.Label lblDatum;
+        private System.Windows.Forms.Label lblImeiPrezime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNazivDodajSuplement;
+        private System.Windows.Forms.TextBox txtCijenaDodajSuplement;
+        private System.Windows.Forms.TextBox txtKolicinaDodajSuplement;
+    }
+}
---
+++ Software/eGym/eGym/FormaDodajSuplement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace eGym
+{
+    public partial class FormaDodajSuplement : Form
+    {
+        public FormaDodajSuplement()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNatragDodajSuplement_Click(object sender, EventArgs e)
+        {
+            FormaEvidencijaSuplemenata formaEvidencijaSuplemenata = new FormaEvidencijaSuplemenata();
+            formaEvidencijaSuplemenata.Show();
+            this.Hide();
+        }
+
+        private void btnDodajSuplementD_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Uspješno ste dodali suplement!");
+            FormaEvidencijaSuplemenata formaEvidencijaSuplemenata = new FormaEvidencijaSuplemenata();
+            formaEvidencijaSuplemenata.Show();
+            this.Hide();
+        }
+    }
+}
