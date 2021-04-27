1d188c9b76b7ecfc3851901bcfbc8bc0d462b2da
lmiroic1 <lmiroic1@foi.hr>
2021-04-27 14:45:08 +0200

    Implementiran dizajn forme FormaUrediProfilZaposlenika.

---
+++ Software/eGym/eGym/FormaUrediProfilZaposlenika.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace eGym
+{
+    public partial class FormaUrediProfilZaposlenika : Form
+    {
+        public FormaUrediProfilZaposlenika()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNatrahUrediProfilZaposlenika_Click(object sender, EventArgs e)
+        {
+            FormProfilZaposlenika formProfilZaposlenika = new FormProfilZaposlenika();
+            formProfilZaposlenika.Show();
+            this.Hide();
+        }
+
+        private void btnSpremiUrediProfilZaposlenika_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Uspješno ste spremili promjene!");
+        }
+    }
+}
