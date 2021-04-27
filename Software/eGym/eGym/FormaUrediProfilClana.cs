cc3281fb6bd60472cadc1e8a4aad43d136d8e868
lmiroic1 <lmiroic1@foi.hr>
2021-04-27 14:45:45 +0200

    Implementiran dizajn forme FormaUrediProfilClana.

---
+++ Software/eGym/eGym/FormaUrediProfilClana.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace eGym
+{
+    public partial class FormaUrediProfilClana : Form
+    {
+        public FormaUrediProfilClana()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSpremiUrediProfilClana_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Uspješno ste spremili promjene!");
+        }
+
+        private void btnNatragUrediProfilClana_Click(object sender, EventArgs e)
+        {
+            formProfilClana formaProfilClana = new formProfilClana();
+            formaProfilClana.Show();
+            this.Hide();
+        }
+    }
+}
