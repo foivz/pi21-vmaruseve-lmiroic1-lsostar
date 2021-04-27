debf42c1a1b7295865e25b15bdf3d423f62027c1
ValentinaMarusevec1 <vmaruseve@foi.hr>
2021-04-27 16:10:46 +0200

    Implementirani dizajn forme EvidencijeTermina.

---
+++ Software/eGym/eGym/FormaEvidencijaTermina.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace eGym
+{
+    partial class FormaEvidencijaTermina
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvEvidencijaTermina = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnObrisiTermin = new System.Windows.Forms.Button();
+            this.btnAzurirajTerminE = new System.Windows.Forms.Button();
+            this.btnNatragSuplement = new System.Windows.Forms.Button();
+            this.btnDodajTerminE = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEvidencijaTermina)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvEvidencijaTermina
+            // 
+            this.dgvEvidencijaTermina.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEvidencijaTermina.Location = new System.Drawing.Point(83, 158);
+            this.dgvEvidencijaTermina.Name = "dgvEvidencijaTermina";
+            this.dgvEvidencijaTermina.RowHeadersWidth = 51;
+            this.dgvEvidencijaTermina.RowTemplate.Height = 24;
+            this.dgvEvidencijaTermina.Size = new System.Drawing.Size(800, 298);
+            this.dgvEvidencijaTermina.TabIndex = 81;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(364, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 29);
+            this.label1.TabIndex = 80;
+            this.label1.Text = "Evidencija termina";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(73, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 55);
+            this.label2.TabIndex = 79;
+            this.label2.Text = "eGym";
+            // 
+            // btnObrisiTermin
+            // 
+            this.btnObrisiTermin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnObrisiTermin.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnObrisiTermin.ForeColor = System.Drawing.Color.White;
+            this.btnObrisiTermin.Location = new System.Drawing.Point(755, 478);
+            this.btnObrisiTermin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnObrisiTermin.Name = "btnObrisiTermin";
+            this.btnObrisiTermin.Size = new System.Drawing.Size(115, 52);
+            this.btnObrisiTermin.TabIndex = 85;
+            this.btnObrisiTermin.Text = "Obriši termin";
+            this.btnObrisiTermin.UseVisualStyleBackColor = false;
+            this.btnObrisiTermin.Click += new System.EventHandler(this.btnObrisiTermin_Click);
+            // 
+            // btnAzurirajTerminE
+            // 
+            this.btnAzurirajTerminE.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnAzurirajTerminE.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAzurirajTerminE.ForeColor = System.Drawing.Color.White;
+            this.btnAzurirajTerminE.Location = new System.Drawing.Point(621, 480);
+            this.btnAzurirajTerminE.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnAzurirajTerminE.Name = "btnAzurirajTerminE";
+            this.btnAzurirajTerminE.Size = new System.Drawing.Size(115, 50);
+            this.btnAzurirajTerminE.TabIndex = 84;
+            this.btnAzurirajTerminE.Text = "Ažuriraj termin";
+            this.btnAzurirajTerminE.UseVisualStyleBackColor = false;
+            this.btnAzurirajTerminE.Click += new System.EventHandler(this.btnAzurirajTerminE_Click);
+            // 
+            // btnNatragSuplement
+            // 
+            this.btnNatragSuplement.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnNatragSuplement.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNatragSuplement.ForeColor = System.Drawing.Color.White;
+            this.btnNatragSuplement.Location = new System.Drawing.Point(87, 480);
+            this.btnNatragSuplement.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnNatragSuplement.Name = "btnNatragSuplement";
+            this.btnNatragSuplement.Size = new System.Drawing.Size(115, 50);
+            this.btnNatragSuplement.TabIndex = 82;
+            this.btnNatragSuplement.Text = "Natrag";
+            this.btnNatragSuplement.UseVisualStyleBackColor = false;
+            this.btnNatragSuplement.Click += new System.EventHandler(this.btnNatragSuplement_Click);
+            // 
+            // btnDodajTerminE
+            // 
+            this.btnDodajTerminE.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnDodajTerminE.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDodajTerminE.ForeColor = System.Drawing.Color.White;
+            this.btnDodajTerminE.Location = new System.Drawing.Point(487, 480);
+            this.btnDodajTerminE.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnDodajTerminE.Name = "btnDodajTerminE";
+            this.btnDodajTerminE.Size = new System.Drawing.Size(115, 50);
+            this.btnDodajTerminE.TabIndex = 83;
+            this.btnDodajTerminE.Text = "Dodaj termin";
+            this.btnDodajTerminE.UseVisualStyleBackColor = false;
+            this.btnDodajTerminE.Click += new System.EventHandler(this.btnDodajTerminE_Click);
+            // 
+            // FormaEvidencijaTermina
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(970, 589);
+            this.Controls.Add(this.btnObrisiTermin);
+            this.Controls.Add(this.btnAzurirajTerminE);
+            this.Controls.Add(this.btnNatragSuplement);
+            this.Controls.Add(this.btnDodajTerminE);
+            this.Controls.Add(this.dgvEvidencijaTermina);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Name = "FormaEvidencijaTermina";
+            this.Text = "Evidencija termina";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEvidencijaTermina)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvEvidencijaTermina;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnObrisiTermin;
+        private System.Windows.Forms.Button btnAzurirajTerminE;
+        private System.Windows.Forms.Button btnNatragSuplement;
+        private System.Windows.Forms.Button btnDodajTerminE;
+    }
+}
---
+++ Software/eGym/eGym/FormaEvidencijaTermina.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace eGym
+{
+    public partial class FormaEvidencijaTermina : Form
+    {
+        public FormaEvidencijaTermina()
+        {
+            InitializeComponent();
+        }
+
+        private void btnObrisiTermin_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Uspješno obrisan termin!");
+        }
+
+        private void btnNatragSuplement_Click(object sender, EventArgs e)
+        {
+            FormProfilZaposlenika formProfilZaposlenika = new FormProfilZaposlenika();
+            formProfilZaposlenika.Show();
+            this.Hide();
+        }
+
+        private void btnDodajTerminE_Click(object sender, EventArgs e)
+        {
+            FormaDodajTermin formaDodajTermin = new FormaDodajTermin();
+            formaDodajTermin.Show();
+            this.Hide();
+        }
+
+        private void btnAzurirajTerminE_Click(object sender, EventArgs e)
+        {
+            FormaAzurirajTermin formaAzurirajTermin = new FormaAzurirajTermin();
+            formaAzurirajTermin.Show();
+            this.Hide();
+        }
+    }
+}
