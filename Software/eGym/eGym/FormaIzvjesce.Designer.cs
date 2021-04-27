8e98ac821ea6eb8486e91ded2051eb402dc2b89a
lmiroic1 <lmiroic1@foi.hr>
2021-04-27 14:46:09 +0200

    Implementiran dizajn forme FormaIzvjesce.

---
+++ Software/eGym/eGym/FormaIzvjesce.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace eGym
+{
+    partial class FormaIzvjesce
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.btnNatragIzvjesce = new System.Windows.Forms.Button();
+            this.btnPrintajIzvjesce = new System.Windows.Forms.Button();
+            this.btnIzvjeziIzvjesce = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(51, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 55);
+            this.label2.TabIndex = 85;
+            this.label2.Text = "eGym";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(61, 138);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(336, 248);
+            this.dataGridView1.TabIndex = 86;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(471, 138);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(336, 248);
+            this.dataGridView2.TabIndex = 87;
+            // 
+            // btnNatragIzvjesce
+            // 
+            this.btnNatragIzvjesce.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnNatragIzvjesce.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNatragIzvjesce.ForeColor = System.Drawing.Color.White;
+            this.btnNatragIzvjesce.Location = new System.Drawing.Point(61, 438);
+            this.btnNatragIzvjesce.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnNatragIzvjesce.Name = "btnNatragIzvjesce";
+            this.btnNatragIzvjesce.Size = new System.Drawing.Size(117, 50);
+            this.btnNatragIzvjesce.TabIndex = 92;
+            this.btnNatragIzvjesce.Text = "Natrag";
+            this.btnNatragIzvjesce.UseVisualStyleBackColor = false;
+            this.btnNatragIzvjesce.Click += new System.EventHandler(this.btnNatragIzvjesce_Click);
+            // 
+            // btnPrintajIzvjesce
+            // 
+            this.btnPrintajIzvjesce.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnPrintajIzvjesce.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrintajIzvjesce.ForeColor = System.Drawing.Color.White;
+            this.btnPrintajIzvjesce.Location = new System.Drawing.Point(550, 438);
+            this.btnPrintajIzvjesce.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnPrintajIzvjesce.Name = "btnPrintajIzvjesce";
+            this.btnPrintajIzvjesce.Size = new System.Drawing.Size(117, 50);
+            this.btnPrintajIzvjesce.TabIndex = 93;
+            this.btnPrintajIzvjesce.Text = "Printaj izvješće";
+            this.btnPrintajIzvjesce.UseVisualStyleBackColor = false;
+            // 
+            // btnIzvjeziIzvjesce
+            // 
+            this.btnIzvjeziIzvjesce.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnIzvjeziIzvjesce.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIzvjeziIzvjesce.ForeColor = System.Drawing.Color.White;
+            this.btnIzvjeziIzvjesce.Location = new System.Drawing.Point(690, 438);
+            this.btnIzvjeziIzvjesce.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnIzvjeziIzvjesce.Name = "btnIzvjeziIzvjesce";
+            this.btnIzvjeziIzvjesce.Size = new System.Drawing.Size(117, 50);
+            this.btnIzvjeziIzvjesce.TabIndex = 94;
+            this.btnIzvjeziIzvjesce.Text = "Izvezi izvješće";
+            this.btnIzvjeziIzvjesce.UseVisualStyleBackColor = false;
+            // 
+            // FormaIzvjesce
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 559);
+            this.Controls.Add(this.btnIzvjeziIzvjesce);
+            this.Controls.Add(this.btnPrintajIzvjesce);
+            this.Controls.Add(this.btnNatragIzvjesce);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Name = "FormaIzvjesce";
+            this.Text = "FormaIzvjesce";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button btnNatragIzvjesce;
+        private System.Windows.Forms.Button btnPrintajIzvjesce;
+        private System.Windows.Forms.Button btnIzvjeziIzvjesce;
+    }
+}
