10efe205acaabfec4c74040f8ed78e82298f694d
lmiroic1 <lmiroic1@foi.hr>
2021-04-27 14:48:27 +0200

    Implementiran dizajn forme azuriranja suplementa.

---
+++ Software/eGym/eGym/FormaAzurirajSuplement.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace eGym
+{
+    partial class FormaAzurirajSuplement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtKolicinaAzurirajSuplement = new System.Windows.Forms.TextBox();
+            this.txtCijenaAzurirajSuplement = new System.Windows.Forms.TextBox();
+            this.txtNazivAzurirajSuplement = new System.Windows.Forms.TextBox();
+            this.btnAzurirajSuplementA = new System.Windows.Forms.Button();
+            this.btnNatragAzurirajSuplement = new System.Windows.Forms.Button();
+            this.lblRadnoVrijeme = new System.Windows.Forms.Label();
+            this.lblDatum = new System.Windows.Forms.Label();
+            this.lblImeiPrezime = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtKolicinaAzurirajSuplement
+            // 
+            this.txtKolicinaAzurirajSuplement.Location = new System.Drawing.Point(225, 266);
+            this.txtKolicinaAzurirajSuplement.Name = "txtKolicinaAzurirajSuplement";
+            this.txtKolicinaAzurirajSuplement.Size = new System.Drawing.Size(218, 22);
+            this.txtKolicinaAzurirajSuplement.TabIndex = 72;
+            // 
+            // txtCijenaAzurirajSuplement
+            // 
+            this.txtCijenaAzurirajSuplement.Location = new System.Drawing.Point(225, 227);
+            this.txtCijenaAzurirajSuplement.Name = "txtCijenaAzurirajSuplement";
+            this.txtCijenaAzurirajSuplement.Size = new System.Drawing.Size(218, 22);
+            this.txtCijenaAzurirajSuplement.TabIndex = 71;
+            // 
+            // txtNazivAzurirajSuplement
+            // 
+            this.txtNazivAzurirajSuplement.Location = new System.Drawing.Point(225, 188);
+            this.txtNazivAzurirajSuplement.Name = "txtNazivAzurirajSuplement";
+            this.txtNazivAzurirajSuplement.Size = new System.Drawing.Size(218, 22);
+            this.txtNazivAzurirajSuplement.TabIndex = 70;
+            // 
+            // btnAzurirajSuplementA
+            // 
+            this.btnAzurirajSuplementA.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnAzurirajSuplementA.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAzurirajSuplementA.ForeColor = System.Drawing.Color.White;
+            this.btnAzurirajSuplementA.Location = new System.Drawing.Point(321, 345);
+            this.btnAzurirajSuplementA.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnAzurirajSuplementA.Name = "btnAzurirajSuplementA";
+            this.btnAzurirajSuplementA.Size = new System.Drawing.Size(122, 50);
+            this.btnAzurirajSuplementA.TabIndex = 69;
+            this.btnAzurirajSuplementA.Text = "Ažuriraj suplement";
+            this.btnAzurirajSuplementA.UseVisualStyleBackColor = false;
+            this.btnAzurirajSuplementA.Click += new System.EventHandler(this.btnAzurirajSuplementA_Click);
+            // 
+            // btnNatragAzurirajSuplement
+            // 
+            this.btnNatragAzurirajSuplement.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnNatragAzurirajSuplement.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNatragAzurirajSuplement.ForeColor = System.Drawing.Color.White;
+            this.btnNatragAzurirajSuplement.Location = new System.Drawing.Point(102, 345);
+            this.btnNatragAzurirajSuplement.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnNatragAzurirajSuplement.Name = "btnNatragAzurirajSuplement";
+            this.btnNatragAzurirajSuplement.Size = new System.Drawing.Size(117, 50);
+            this.btnNatragAzurirajSuplement.TabIndex = 68;
+            this.btnNatragAzurirajSuplement.Text = "Natrag";
+            this.btnNatragAzurirajSuplement.UseVisualStyleBackColor = false;
+            this.btnNatragAzurirajSuplement.Click += new System.EventHandler(this.btnNatragAzurirajSuplement_Click);
+            // 
+            // lblRadnoVrijeme
+            // 
+            this.lblRadnoVrijeme.AutoSize = true;
+            this.lblRadnoVrijeme.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRadnoVrijeme.Location = new System.Drawing.Point(108, 266);
+            this.lblRadnoVrijeme.Name = "lblRadnoVrijeme";
+            this.lblRadnoVrijeme.Size = new System.Drawing.Size(64, 18);
+            this.lblRadnoVrijeme.TabIndex = 67;
+            this.lblRadnoVrijeme.Text = "Količina:";
+            // 
+            // lblDatum
+            // 
+            this.lblDatum.AutoSize = true;
+            this.lblDatum.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDatum.Location = new System.Drawing.Point(119, 227);
+            this.lblDatum.Name = "lblDatum";
+            this.lblDatum.Size = new System.Drawing.Size(53, 18);
+            this.lblDatum.TabIndex = 66;
+            this.lblDatum.Text = "Cijena:";
+            // 
+            // lblImeiPrezime
+            // 
+            this.lblImeiPrezime.AutoSize = true;
+            this.lblImeiPrezime.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImeiPrezime.Location = new System.Drawing.Point(123, 188);
+            this.lblImeiPrezime.Name = "lblImeiPrezime";
+            this.lblImeiPrezime.Size = new System.Drawing.Size(49, 18);
+            this.lblImeiPrezime.TabIndex = 65;
+            this.lblImeiPrezime.Text = "Naziv:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(121, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 29);
+            this.label1.TabIndex = 64;
+            this.label1.Text = "Ažuriraj suplement";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(74, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 55);
+            this.label2.TabIndex = 63;
+            this.label2.Text = "eGym";
+            // 
+            // FormaAzurirajSuplement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 450);
+            this.Controls.Add(this.txtKolicinaAzurirajSuplement);
+            this.Controls.Add(this.txtCijenaAzurirajSuplement);
+            this.Controls.Add(this.txtNazivAzurirajSuplement);
+            this.Controls.Add(this.btnAzurirajSuplementA);
+            this.Controls.Add(this.btnNatragAzurirajSuplement);
+            this.Controls.Add(this.lblRadnoVrijeme);
+            this.Controls.Add(this.lblDatum);
+            this.Controls.Add(this.lblImeiPrezime);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Name = "FormaAzurirajSuplement";
+            this.Text = "AzurirajSuplement";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtKolicinaAzurirajSuplement;
+        private System.Windows.Forms.TextBox txtCijenaAzurirajSuplement;
+        private System.Windows.Forms.TextBox txtNazivAzurirajSuplement;
+        private System.Windows.Forms.Button btnAzurirajSuplementA;
+        private System.Windows.Forms.Button btnNatragAzurirajSuplement;
+        private System.Windows.Forms.Label lblRadnoVrijeme;
+        private System.Windows.Forms.Label lblDatum;
+        private System.Windows.Forms.Label lblImeiPrezime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Software/eGym/eGym/FormaAzurirajSuplement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace eGym
+{
+    public partial class FormaAzurirajSuplement : Form
+    {
+        public FormaAzurirajSuplement()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNatragAzurirajSuplement_Click(object sender, EventArgs e)
+        {
+            FormaEvidencijaSuplemenata formaEvidencijaSuplemenata = new FormaEvidencijaSuplemenata();
+            formaEvidencijaSuplemenata.Show();
+            this.Hide();
+        }
+
+        private void btnAzurirajSuplementA_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Uspješno ste ažurirali suplement!");
+            FormaEvidencijaSuplemenata formaEvidencijaSuplemenata = new FormaEvidencijaSuplemenata();
+            formaEvidencijaSuplemenata.Show();
+            this.Hide();
+        }
+    }
+}
