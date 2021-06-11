98a98816d8ef141f21d09eba0155204a7a0677a8
lmiroic1 <lmiroic1@foi.hr>
2021-06-11 22:54:58 +0200

    Azuriranje clana.

--- Software/eGym/eGym/FormaAzurirajClana.cs
+++ Software/eGym/eGym/FormaAzurirajClana.cs
@@ -21,6 +21,24 @@ namespace eGym
 
         private void btnAzurirajClanaA_Click(object sender, EventArgs e)
         {
+            using (var context = new Entities_())
+            {
+                context.Korisniks.Attach(OdabraniKorisnik);
+                OdabraniKorisnik.brojtelefona = txtBrojTelefonaClanaA.Text;
+                OdabraniKorisnik.ime = txtImeClanaA.Text;
+                OdabraniKorisnik.prezime = txtPrezimeClanaA.Text;
+                OdabraniKorisnik.email = txtEmailClanaA.Text;             
+
+
+                NovacKorisnika novacKorisnika = (from nk in context.NovacKorisnikas
+                                                 where nk.korisnik_korisnickoIme == OdabraniKorisnik.korisnickoIme
+                                                 select nk).FirstOrDefault();
+                context.NovacKorisnikas.Attach(novacKorisnika);
+                novacKorisnika.stanjeNaRacunu = decimal.Parse(txtIznosNaRacunuA.Text);
+                context.SaveChanges();
+
+
+            }
             MessageBox.Show("Uspješno ste ažurirali člana!");
             FormaEvidencijaClanova formaEvidencijaClanova = new FormaEvidencijaClanova();
             formaEvidencijaClanova.Show();
@@ -44,14 +62,17 @@ namespace eGym
             cbClanarinaPlacenaAzurirajClana.Items.Add("Ne");
             if (OdabraniKorisnik != null)
             {
-                using (var context = new Entities_())
+                NovacKorisnika novack = null;
+                using(var context=new Entities_())
                 {
-                    var query1 = from nk in context.NovacKorisnikas.Include("Korisnik")
-                                where nk.korisnik_korisnickoIme == OdabraniKorisnik.korisnickoIme
-                                select new NovacKorisnikaView { StanjeNaRacunu = nk.stanjeNaRacunu };
-                    //lblIznosNaRacunu.Text = query1.FirstOrDefault().StanjeNaRacunu.ToString();
-
+                    var novacKorisnika = (from nk in context.NovacKorisnikas
+                                                     where nk.korisnik_korisnickoIme.Equals(OdabraniKorisnik.korisnickoIme)
+                                                     select nk).FirstOrDefault();
+                    novack = novacKorisnika;
                 }
+                txtIznosNaRacunuA.Text = novack.stanjeNaRacunu.ToString();
+
+                
             }
 
 
