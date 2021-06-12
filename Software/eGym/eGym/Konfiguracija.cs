ceabc6e7d5a9fd493b1d4121948033e3eeee94db
lmiroic1 <lmiroic1@foi.hr>
2021-06-12 19:04:39 +0200

    Implementacija klase Konfiguracija.

---
+++ Software/eGym/eGym/Konfiguracija.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eGym
+{
+    public class Konfiguracija
+    {
+        public static string VratiPostavku(string kljuc)
+        {
+            return ConfigurationManager.AppSettings.Get(kljuc);
+        }
+    }
+}
