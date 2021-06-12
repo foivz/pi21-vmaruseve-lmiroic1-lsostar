30e63d07fad481175caad53ba9ae9963112b35eb
lmiroic1 <lmiroic1@foi.hr>
2021-06-12 14:52:33 +0200

    Implementirana rezervacija individualnih treninga s odabirom trenera.

--- Software/eGym/eGym/FormIndividualniTrening.Designer.cs
+++ Software/eGym/eGym/FormIndividualniTrening.Designer.cs
@@ -46,10 +46,10 @@ namespace eGym
             this.btnRezervirajIT.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.btnRezervirajIT.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnRezervirajIT.ForeColor = System.Drawing.Color.White;
-            this.btnRezervirajIT.Location = new System.Drawing.Point(503, 358);
-            this.btnRezervirajIT.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnRezervirajIT.Location = new System.Drawing.Point(671, 441);
+            this.btnRezervirajIT.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnRezervirajIT.Name = "btnRezervirajIT";
-            this.btnRezervirajIT.Size = new System.Drawing.Size(92, 41);
+            this.btnRezervirajIT.Size = new System.Drawing.Size(123, 50);
             this.btnRezervirajIT.TabIndex = 92;
             this.btnRezervirajIT.Text = "Rezerviraj";
             this.btnRezervirajIT.UseVisualStyleBackColor = false;
@@ -60,10 +60,10 @@ namespace eGym
             this.btnNatragIT.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.btnNatragIT.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnNatragIT.ForeColor = System.Drawing.Color.White;
-            this.btnNatragIT.Location = new System.Drawing.Point(45, 358);
-            this.btnNatragIT.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnNatragIT.Location = new System.Drawing.Point(60, 441);
+            this.btnNatragIT.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnNatragIT.Name = "btnNatragIT";
-            this.btnNatragIT.Size = new System.Drawing.Size(88, 41);
+            this.btnNatragIT.Size = new System.Drawing.Size(117, 50);
             this.btnNatragIT.TabIndex = 91;
             this.btnNatragIT.Text = "Natrag";
             this.btnNatragIT.UseVisualStyleBackColor = false;
@@ -71,60 +71,59 @@ namespace eGym
             // 
             // dtpOdaberiDatumIndividualni
             // 
-            this.dtpOdaberiDatumIndividualni.Location = new System.Drawing.Point(172, 310);
-            this.dtpOdaberiDatumIndividualni.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dtpOdaberiDatumIndividualni.Location = new System.Drawing.Point(229, 382);
+            this.dtpOdaberiDatumIndividualni.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dtpOdaberiDatumIndividualni.Name = "dtpOdaberiDatumIndividualni";
-            this.dtpOdaberiDatumIndividualni.Size = new System.Drawing.Size(177, 20);
+            this.dtpOdaberiDatumIndividualni.Size = new System.Drawing.Size(235, 22);
             this.dtpOdaberiDatumIndividualni.TabIndex = 90;
+            this.dtpOdaberiDatumIndividualni.ValueChanged += new System.EventHandler(this.dtpOdaberiDatumIndividualni_ValueChanged);
             // 
             // cmbOdabirTrenera
             // 
             this.cmbOdabirTrenera.FormattingEnabled = true;
-            this.cmbOdabirTrenera.Location = new System.Drawing.Point(172, 280);
-            this.cmbOdabirTrenera.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cmbOdabirTrenera.Location = new System.Drawing.Point(229, 345);
+            this.cmbOdabirTrenera.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cmbOdabirTrenera.Name = "cmbOdabirTrenera";
-            this.cmbOdabirTrenera.Size = new System.Drawing.Size(177, 21);
+            this.cmbOdabirTrenera.Size = new System.Drawing.Size(235, 24);
             this.cmbOdabirTrenera.TabIndex = 89;
+            this.cmbOdabirTrenera.SelectedIndexChanged += new System.EventHandler(this.cmbOdabirTrenera_SelectedIndexChanged);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(79, 310);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(105, 382);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.Size = new System.Drawing.Size(106, 17);
             this.label4.TabIndex = 87;
             this.label4.Text = "Odaberi datum:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(74, 282);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(99, 347);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.Size = new System.Drawing.Size(113, 17);
             this.label3.TabIndex = 88;
             this.label3.Text = "Odaberi trenera:";
             // 
             // dgvIndividualniTrening
             // 
             this.dgvIndividualniTrening.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvIndividualniTrening.Location = new System.Drawing.Point(45, 114);
-            this.dgvIndividualniTrening.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvIndividualniTrening.Location = new System.Drawing.Point(60, 140);
+            this.dgvIndividualniTrening.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dgvIndividualniTrening.Name = "dgvIndividualniTrening";
             this.dgvIndividualniTrening.RowHeadersWidth = 51;
             this.dgvIndividualniTrening.RowTemplate.Height = 24;
-            this.dgvIndividualniTrening.Size = new System.Drawing.Size(550, 145);
+            this.dgvIndividualniTrening.Size = new System.Drawing.Size(733, 178);
             this.dgvIndividualniTrening.TabIndex = 86;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(250, 72);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(333, 89);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(173, 25);
+            this.label1.Size = new System.Drawing.Size(224, 29);
             this.label1.TabIndex = 85;
             this.label1.Text = "Individualni trening";
             // 
@@ -133,18 +132,17 @@ namespace eGym
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.label2.Location = new System.Drawing.Point(25, 15);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(33, 18);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(123, 44);
+            this.label2.Size = new System.Drawing.Size(152, 55);
             this.label2.TabIndex = 84;
             this.label2.Text = "eGym";
             // 
             // FormIndividualniTrening
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(638, 437);
+            this.ClientSize = new System.Drawing.Size(851, 538);
             this.Controls.Add(this.btnRezervirajIT);
             this.Controls.Add(this.btnNatragIT);
             this.Controls.Add(this.dtpOdaberiDatumIndividualni);
@@ -154,7 +152,7 @@ namespace eGym
             this.Controls.Add(this.dgvIndividualniTrening);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormIndividualniTrening";
             this.Text = "IndividualniTrening";
             this.Load += new System.EventHandler(this.FormIndividualniTrening_Load);
--- Software/eGym/eGym/FormIndividualniTrening.cs
+++ Software/eGym/eGym/FormIndividualniTrening.cs
@@ -12,6 +12,7 @@ namespace eGym
 {
     public partial class FormIndividualniTrening : Form
     {
+        
         public FormIndividualniTrening()
         {
             InitializeComponent();
@@ -19,6 +20,16 @@ namespace eGym
 
         private void btnRezervirajIT_Click(object sender, EventArgs e)
         {
+            Termin odabraniTermin = dgvIndividualniTrening.CurrentRow.DataBoundItem as Termin;
+            using (var context = new Entities6())
+            {
+                Termin query = (from t in context.Termins
+                             where t.ID==odabraniTermin.ID
+                             select t).SingleOrDefault();
+                context.Termins.Attach(query);
+                query.broj_mjesta = query.broj_mjesta - 1;
+                context.SaveChanges();
+            }
             MessageBox.Show("Uspješno rezerviran trening!");
         }
 
@@ -31,7 +42,51 @@ namespace eGym
 
         private void FormIndividualniTrening_Load(object sender, EventArgs e)
         {
+            List<Korisnik> treneri = new List<Korisnik>();
+            using (var context = new Entities6())
+            {
+                var query = (from k in context.Korisniks
+                             where k.uloga_id == 2
+                             select k).ToList();
+                treneri = query;
+            }
+            cmbOdabirTrenera.DataSource = null;
+            cmbOdabirTrenera.DataSource = treneri;
+            cmbOdabirTrenera.DisplayMember = "ime" + "prezime";
+            cmbOdabirTrenera.ValueMember = "korisnickoIme";
 
+        }
+
+        private void cmbOdabirTrenera_SelectedIndexChanged(object sender, EventArgs e)
+        {           
+            Korisnik trener = cmbOdabirTrenera.SelectedItem as Korisnik;
+            DateTime zeljenidatum = dtpOdaberiDatumIndividualni.Value.Date;
+            List<Termin> terminiIndividualnihTreninga = new List<Termin>();
+            using (var context = new Entities6())
+            {
+                var query = (from t in context.Termins
+                             where t.zaposlenik_korisnickoIme == trener.korisnickoIme && t.vrstaVjezbe_id == 1 && t.od.Month == zeljenidatum.Month && t.od.Day == zeljenidatum.Day&&t.broj_mjesta>=1
+                             select t).ToList();
+                terminiIndividualnihTreninga = query;
+            }
+            dgvIndividualniTrening.DataSource = null;
+            dgvIndividualniTrening.DataSource = terminiIndividualnihTreninga;
+        }
+
+        private void dtpOdaberiDatumIndividualni_ValueChanged(object sender, EventArgs e)
+        {
+            Korisnik trener = cmbOdabirTrenera.SelectedItem as Korisnik;
+            DateTime zeljenidatum = dtpOdaberiDatumIndividualni.Value.Date;
+            List<Termin> terminiIndividualnihTreninga = new List<Termin>();
+            using (var context = new Entities6())
+            {
+                var query = (from t in context.Termins
+                             where t.zaposlenik_korisnickoIme == trener.korisnickoIme && t.vrstaVjezbe_id == 1 && t.od.Month== zeljenidatum.Month&&t.od.Day==zeljenidatum.Day&&t.broj_mjesta>=1
+                             select t).ToList();
+                terminiIndividualnihTreninga = query;
+            }
+            dgvIndividualniTrening.DataSource = null;
+            dgvIndividualniTrening.DataSource = terminiIndividualnihTreninga;
         }
     }
 }
